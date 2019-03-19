c08945ba582947ad01a5ed3f7d1de842c566b1be
seungsukim <seungsu0407@gmail.com>
2019-03-19 15:43:34 +0900

    Test done

--- maskrcnn-benchmark/demo/test.py
+++ maskrcnn-benchmark/demo/test.py
@@ -1,7 +1,9 @@
+
 import argparse
+import cv2
 
 from maskrcnn_benchmark.config import cfg
-from predictor import COCODemo
+from demo.predictor import COCODemo
 
 import time
 
@@ -9,15 +11,21 @@ import time
 def main():
     
     cfg.merge_from_file("../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml")
-    cfg.merge_from_list(None)
+    #cfg.merge_from_list(None)
     cfg.freeze()
 
     coco_demo = COCODemo(
         cfg,
         confidence_threshold=0.7,
-        show_mask_heaptamps=False,
-        mask_per_dim=2,
+        show_mask_heatmaps=False,
+        masks_per_dim=2,
         min_image_size=224,
     )
 
+    img = cv2.imread("test.jpg")
+    composite = coco_demo.run_on_opencv_image(img)
+    print(composite is None)
+    cv2.imwrite("./result.jpg", composite)
 
+if __name__=='__main__':
+    main()
